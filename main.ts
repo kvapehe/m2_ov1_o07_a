0be5b1c7dc51b9084a9444068e6106a330e12b68
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-12 15:50:57 +0100

    Leveringsklart program med ulike bilder basert på hvordan Microbit håndteres i shake-modus

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
-
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # . . .
+        . # . . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . . # . .
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        . . . # #
+        . . . # .
+        `)
+})
+basic.forever(function () {
+	
+})
